127db29e548f4b697db350bfb6e8144e0acb763e
MarcosTc3 <marcostoribiocano8@gmail.com>
2025-10-08 22:56:19 +0200

    feat: Añadir componente PromoBanner personalizado a la página de inicio

---
+++ client/src/components/PromoBanner/PromoBanner.jsx
@@ -0,0 +1,17 @@
+import './PromoBanner.css';
+
+function PromoBanner() {
+  return (
+    <section className="promo-banner-section">
+      <div className="container">
+        <div className="promo-content">
+          <h2 className="promo-title">Calidad y Asesoramiento</h2>
+          <p className="promo-subtitle">Nuestra seña de identidad</p>
+          <button className="promo-button">Saber Más</button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default PromoBanner;
--- client/src/pages/Inicio.jsx
+++ client/src/pages/Inicio.jsx
@@ -1,13 +1,13 @@
-// src/pages/Inicio.jsx
 import Hero from '../components/Hero/Hero';
 import KeyServices from '../components/KeyServices/KeyServices';
+import PromoBanner from '../components/PromoBanner/PromoBanner';
 
 function Inicio() {
   return (
     <>
       <Hero />
       <KeyServices />
-      {/* Aquí debajo añadiremos más secciones en el futuro, como el banner */}
+      <PromoBanner />
     </>
   );
 }
